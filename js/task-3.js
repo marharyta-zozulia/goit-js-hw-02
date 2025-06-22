f11f937a3f7ab7f891f2dcec000b712d0e58c545
marharyta zozulia <zozulia.marharyta@gmail.com>
2025-06-22 16:50:54 +0200

    js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function checkForSpam(message) {
     const lowerCaseMessage = message.toLowerCase();
-    return lowerCaseMessage.includes("spam"), lowerCaseMessage.includes("sale");
+    return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
